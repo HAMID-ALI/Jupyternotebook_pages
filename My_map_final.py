5eb494b9c3d2cbfeb50d4f97d86cc966e16384e6
HAMID-ALI <40883267+HAMID-ALI@users.noreply.github.com>
2018-11-07 16:53:19 +0530

    Add files via upload

---
+++ My_map_final.py
@@ -0,0 +1,24 @@
+import folium
+import pandas
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat=list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+home_map = folium.Map(location =[26.653363, 85.220949],zoom_start =6 ,tiles="Mapbox Bright")
+fg = folium.FeatureGroup(name="My Map")
+
+def color_producer(elev):
+    if elev < 1000:
+        return "green"
+    elif elev < 2000:
+        return "blue"
+    else:
+        return "red"
+    
+
+for lt, ln, el in zip(lat,lon,elev):
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup= str(el)+" m", fill_color = color_producer(el), color = 'grey',opacity    = 0.7))
+    
+fg.add_child(folium.GeoJson(data = open("115 world.json", encoding ='utf-8-sig').read(), style_function =lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+home_map.add_child(fg)
+home_map.save("my_home_map.html")
