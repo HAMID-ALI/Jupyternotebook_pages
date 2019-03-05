7e3f287d248a13ed76edf3193aa3bd11b92c72d6
HAMID-ALI <40883267+HAMID-ALI@users.noreply.github.com>
2019-03-05 11:49:28 +0530

    Update Exception_handl.py

--- Exception_handl.py
+++ Exception_handl.py
@@ -2,7 +2,7 @@ def divide(a,b):
     try:
         return(a/b)
     except:
-        print("Division by zero not defined")
+        return"Division by zero not defined"
 a=4
 b=int(input("Enter vale of b"))
 divide(a,b)
